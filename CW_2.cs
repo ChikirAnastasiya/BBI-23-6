d4344ab189e2654f7096500238b4e146edd7427f
ChikirAnastasiya <168197595+ChikirAnastasiya@users.noreply.github.com>
2024-04-26 16:16:28 +0300

    Add files via upload

---
+++ CW_2.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+#region
+abstract class Task
+{
+    protected string text = "No text here yet";
+
+    public string Text
+    {
+        get => text;
+        protected set => text = value;
+    }
+    public Task(string text)
+    { this.text = text;
+    }
+}
+#endregion
+#region
+
+class Task1 : Task
+{
+    [JsonConstructor]
+    public Task1(string text) : base(text) { }
+    public override string ToString()
+    {
+        return text;
+    }
+}
+class Task2: Task
+{
+    private int amount = 1;
+    public int Amount
+    {
+        get => amount;
+        private set => amount = value;
+    }
+    [JsonConstructor]
+    public Task2(string text, int amount = 0): base(text)
+    {
+        this.amount = amount;
+    }
+    public override string ToString ()
+
+}
