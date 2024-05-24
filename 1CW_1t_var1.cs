66d3a10b7c593b3ec3b10ab566b0c13c5a468cfd
ChikirAnastasiya <168197595+ChikirAnastasiya@users.noreply.github.com>
2024-05-24 16:10:51 +0300

    1CW_1-2t_var1

---
+++ 1CW_1t_var1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+struct Number
+{
+    public int Real;
+    public Number(int real)
+    {
+        Real = real;
+    }
+    public static Number Add(Number n1, Number n2)
+    {
+        return new Number(n1.Real + n2.Real);
+    }
+
+    public static Number Substract(Number n1, Number n2)
+    {
+        return new Number(n1.Real - n2.Real);
+    }
+    public static Number Multiply(Number n1, Number n2)
+    {
+        return new Number(n1.Real * n2.Real);
+    }
+    public static Number Divide(Number n1, Number n2)
+    {
+        return new Number(n1.Real / n2.Real);
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Number = {Real}");
+    }
+}
+class Programm
+{
+    static void Main()
+    {
+        Number n1 = new Number(5);
+        Number n2 = new Number(3);
+        Number sum = Number.Add(n1, n2);
+        Number difference = Number.Substract(n1, n2);
+        Number product = Number.Multiply(n1, n2);
+        Number quotient = Number.Divide(n1, n2);
+
+        n1.Print();
+        n2.Print();
+        sum.Print();
+        difference.Print();
+        product.Print();
+        quotient.Print();
+    }
+}
+
+
+
+       
---
+++ 1CW_2t_var1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public struct ComplexNumber
+{
+    private int _realPart;
+    private int _imaginaryPart;
+
+    public ComplexNumber(int realPart, int imaginaryPart)
+    {
+        _realPart = realPart;
+        _imaginaryPart = imaginaryPart;
+    }
+
+    public static void
+PrintComplexNumber(ComplexNumber number)
+    {
+        if (number._imaginaryPart >= 0)
+        {
+            Console.WriteLine($"Number = {number._realPart} + {number._imaginaryPart}");
+        }
+        else
+        {
+            Console.WriteLine($"Number = {number._realPart} - {number._imaginaryPart}");
+        }
+    }
+
+    public static ComplexNumber
+Add(ComplexNumber a, ComplexNumber b)
+    {
+        return new ComplexNumber(a._realPart - b._realPart, a._imaginaryPart - b._imaginaryPart);
+    }
+
+    public static ComplexNumber
+Substract(ComplexNumber a, ComplexNumber b)
+    { 
+        return new ComplexNumber (a._realPart - b._realPart, a._imaginaryPart - b._imaginaryPart);
+    }
+public static ComplexNumber Multiply(ComplexNumber a, ComplexNumber b)
+{
+    int realPart = a._realPart * b._realPart - a._imaginaryPart * b._imaginaryPart;
+    int imaginaryPart = a._realPart * b._imaginaryPart + a._imaginaryPart * b._realPart;
+    return new ComplexNumber(realPart, imaginaryPart);
+}
+public static ComplexNumber Divide(ComplexNumber a, ComplexNumber b)
+{
+    int denominator = b._realPart * b._realPart + b._imaginaryPart * b._imaginaryPart;
+    int realPart = (a._realPart * b._realPart + a._imaginaryPart * b._imaginaryPart) / denominator;
+    int imaginaryPart = (a._imaginaryPart * b._realPart - a._realPart * b._imaginaryPart) / denominator;
+    return new ComplexNumber(realPart, imaginaryPart);
+}
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ComplexNumber c1 = new ComplexNumber(2, 3);
+        ComplexNumber c2 = new ComplexNumber(4, 5);
+        ComplexNumber.PrintComplexNumber(c1);
+        ComplexNumber.PrintComplexNumber(c2);
+        ComplexNumber sum = ComplexNumber.Add(c1, c2);
+        ComplexNumber.PrintComplexNumber(sum);
+        ComplexNumber difference = ComplexNumber.Substract(c1, c2);
+        ComplexNumber.PrintComplexNumber(difference);
+        ComplexNumber product = ComplexNumber.Multiply(c1, c2);
+        ComplexNumber.PrintComplexNumber(product);
+        ComplexNumber quotient = ComplexNumber.Divide(c1, c2);
+        ComplexNumber.PrintComplexNumber(quotient);
+    }
+}
+
